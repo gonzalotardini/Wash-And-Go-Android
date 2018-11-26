c6fe2d004ce33d169eef71066c8ac353ef9494a8
gonzalo <gonzalotardini@gmail.com>
2018-11-26 16:25:14 -0300

    se postea en api grabando movimiento

--- app/src/main/java/com/example/gtardini/washandgo/DetailActivity.java
+++ app/src/main/java/com/example/gtardini/washandgo/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.gtardini.washandgo;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -14,6 +16,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -21,8 +24,11 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class DetailActivity extends AppCompatActivity {
 
+    LavadoDetalle detalle= new LavadoDetalle();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +52,7 @@ public class DetailActivity extends AppCompatActivity {
 
 
     public LavadoDetalle GetDetalle (int id){
-        LavadoDetalle detalle= new LavadoDetalle();
+
     String apilink= "https://washandgo.azurewebsites.net/SolicitarLavado/GetLavadoDetalleAPI?idlavado=" +id;
     StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
     StrictMode.setThreadPolicy(policy);
@@ -100,6 +106,58 @@ public class DetailActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return  detalle;
+    }
+
+
+    public void Aprobar (View view){
+        AprobarLavado(Integer.parseInt(detalle.IdLavado));
+        this.finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void AprobarLavado (int id){
+        String apilink="https://washandgo.azurewebsites.net/SolicitarLavado/AprobarLavado?idlavado="+ id;
+        StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        URL url= null;
+        HttpURLConnection conn;
+
+        try {
+
+            url= new URL(apilink);
+            conn= (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputline;
+
+            StringBuffer response= new StringBuffer();
+            String json="";
+
+            while ((inputline= in.readLine()) !=null){
+                response.append(inputline);
+            }
+
+            json = response.toString();
+
+            JSONObject jsonObj=null;
+            jsonObj= new JSONObject(json);
+
+            Log.d("GONZA", "TERMINO");
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
